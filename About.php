521ec301a3708e6bbedc846dde3aef356aee7d43
Mahabub-1997 <lizukhan13@gmail.com>
2022-03-05 20:42:03 +0600

    05/03/2022

---
+++ About.php
@@ -0,0 +1,201 @@
+ 
+
+<section id="content">
+	<section class="section-padding">
+		<div class="container">
+			<div class="row showcase-section">
+				<div class="col-md-6">
+					<img src="img/dev1.png" alt="showcase image">
+				</div>
+				<div class="col-md-6">
+					<div class="about-text">
+						<h3>Mahabub sit</h3>
+						<p>Thank you for having me. Here I am MD Mahabub Ul Islam. I am a fresh graduate from North South University in Computer Science and Engineering  (CSE)  with 2.95 CGPA. My previous school is Dhanmomndi Boys High School and college is Dhanmondi ideal college. I am from Dhaka and living in Dhanmondi , green road with family . I have a job experience in a gaming farm as an analytical   and idea generator department. Which is not related to my job field. The job type was part time .But now that time I am looking for software engineer which is my ultimate goal of my career..</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<div class="container">
+					
+					<div class="about">
+				
+						
+						<div class="row">
+							<div class="col-md-4">
+								<!-- Heading and para -->
+								<div class="block-heading-two">
+									<h3><span>Why Choose Us?</span></h3>
+								</div>
+								<p>Motivation is a very strong tool we can use to get further in life and to achieve our dreams and goals. Setting goals for ourselves is something that I consider to be very important, I believe in always pushing myself further and further in life, not only physically but mentally as well. We all have different things or tools we can use to motivate us towards our end goals. In order for us to stay motivated we have to have a goal and be driven towards achieving that goal. <br/><br/> For me, self-motivation is empowering. Finding something that I enjoy and love to do and setting the final goal of achieving it.
+</p>
+							</div>
+							<div class="col-md-4">
+								<div class="block-heading-two">
+									<h3><span>Our Solution</span></h3>
+								</div>		
+								<!-- Accordion starts -->
+								<div class="panel-group" id="accordion-alt3">
+								 <!-- Panel. Use "panel-XXX" class for different colors. Replace "XXX" with color. -->
+								  <div class="panel">	
+									<!-- Panel heading -->
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseOne-alt3">
+											<i class="fa fa-angle-right"></i> Accordion Job Creation Statistics  # 1
+										  </a>
+										</h4>
+									 </div>
+									 <div id="collapseOne-alt3" class="panel-collapse collapse">
+										<!-- Panel body -->
+										<div class="panel-body">
+										  hen looking at job creation statistics, remember that not all jobs are created equal. Federal spending on public works creates construction jobs, which will successfully reduce the unemployment rate, but it may not stimulate as much demand as it would if the same number of better paying high-tech jobs were created.In fact, jobs created after the last few recessions have led to greater income inequality because rehired workers became willing to take jobs that paid less
+										</div>
+									 </div>
+								  </div>
+								  <div class="panel">
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseTwo-alt3">
+											<i class="fa fa-angle-right"></i> Accordion Spend on Public Works # 2
+										  </a>
+										</h4>
+									 </div>
+									 <div id="collapseTwo-alt3" class="panel-collapse collapse">
+										<div class="panel-body">
+										A University of Massachusetts at Amherst study found that all government spending is not created equal. The most cost-effective ones are building roads, bridges, and other public works. $1 billion spent on public works created 19,795 jobs. Public works create jobs because it puts people right to work. The federal government can quickly fund construction projects already in the approval pipeline. It can hire contractors, send money to the states, or hire workers directly. That was one reason why the American Recovery and Reinvestment Act ended the Great Recession in 2009.It spent $85 billion in shovel-ready construction projects.
+										</div>
+									 </div>
+								  </div>
+								  <div class="panel">
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseThree-alt3">
+											<i class="fa fa-angle-right"></i>Business Payroll Taxes for New Hires # 3
+										  </a>
+										</h4>
+									 </div>
+									 <div id="collapseThree-alt3" class="panel-collapse collapse">
+										<div class="panel-body">
+										  Tax cuts create jobs by letting families or businesses keep more of the money they earn. The idea is that consumers will buy more things, thereby stimulating demand. Businesses use tax cut money to hire much-needed workers. All tax cuts are not created equal when it comes to job creation, though. For example, a Congressional Budget Office study found that while the Bush tax cuts created 4,600 jobs for every $1 billion in foregone tax revenue, payroll tax cuts did better—they created 13,000 new jobs for every $1 billion spent. 
+										</div>
+									 </div>
+								  </div>
+								  <div class="panel">
+									 <div class="panel-heading">
+										<h4 class="panel-title">
+										  <a data-toggle="collapse" data-parent="#accordion-alt3" href="#collapseFour-alt3">
+											<i class="fa fa-angle-right"></i>  Spend on Unemployment Benefits  # 4
+										  </a>
+										</h4>
+									 </div>
+									 <div id="collapseFour-alt3" class="panel-collapse collapse">
+										<div class="panel-body">
+										   Another cost-effective solution is unemployment benefits. According to a 2010 study, unemployment benefits increased employment on average by 1.6 million jobs each quarter from mid-2008 through mid-2010.8 Unemployment benefits create so many jobs because the unemployed must spend all the benefits received, and they tend to buy necessities such as groceries, clothing, and housing right away. Retailers and manufacturers respond to the added demand by hiring more workers to keep up.These benefits also help keep the unemployed from becoming homeless. It is more difficult for them to find a job if they lose a steady address. 
+										</div>
+									 </div>
+								  </div>
+								</div>
+								<!-- Accordion ends -->
+								
+							</div>
+							
+							<div class="col-md-4">
+								<div class="block-heading-two">
+									<h3><span>Our Expertise</span></h3>
+								</div>								
+								<h6>Web Development</h6>
+								<div class="progress pb-sm">
+								  <!-- White color (progress-bar-white) -->
+								  <div class="progress-bar progress-bar-red" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+									 <span class="sr-only">40% Complete (success)</span>
+								  </div>
+								</div>
+								<h6>Designing</h6>
+								<div class="progress pb-sm">
+								  <div class="progress-bar progress-bar-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+									 <span class="sr-only">40% Complete (success)</span>
+								  </div>
+								</div>
+								<h6>User Experience</h6>
+								<div class="progress pb-sm">
+								  <div class="progress-bar progress-bar-lblue" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+									 <span class="sr-only">40% Complete (success)</span>
+								  </div>
+								</div>
+								<h6>Development</h6>
+								<div class="progress pb-sm">
+								  <div class="progress-bar progress-bar-yellow" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 30%">
+									 <span class="sr-only">40% Complete (success)</span>
+								  </div>
+								</div>
+							</div>
+							
+						</div>
+						
+						 						
+						 
+						<br>
+						<!-- Our Team starts -->
+				
+						<!-- Heading -->
+						<div class="block-heading-six">
+							<h4 class="bg-color">Our Project Team</h4>
+						</div>
+						<br>
+						
+						<!-- Our team starts -->
+						
+						<div class="team-six">
+							<div class="row">
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team1.jpg" alt="">
+										<!-- Name -->
+										<h4>Dr.Anisul Islam</h4>
+										<span class="deg">Supervisor</span> 
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team2.jpg" alt="">
+										<!-- Name -->
+										<h4>MD Mahabub ul Islam</h4>
+										<span class="deg">Programmer One</span> 
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team3.jpg" alt="">
+										<!-- Name -->
+										<h4>Tosmia Islam</h4>
+										<span class="deg">Programmer Two</span> 
+									</div>
+								</div>
+								<div class="col-md-3 col-sm-6">
+									<!-- Team Member -->
+									<div class="team-member">
+										<!-- Image -->
+										<img class="img-responsive" src="img/team4.jpg" alt="">
+										<!-- Name -->
+										<h4>Project Given</h4>
+										<span class="deg">North South University</span> 
+									</div>
+								</div>
+							</div>
+						</div>
+						
+						<!-- Our team ends -->
+					  
+						
+					</div>
+									
+				</div>
+	</section> 
+ 
